4955dc598c586f55f15317fb40c50f15e381997d
vibhanshu <vibhanshu@vibhanshus-MacBook-Air.local>
2017-09-09 15:17:38 +0530

    commit

--- src/main/java/edu/coursera/parallel/OneDimAveragingPhaser.java
+++ src/main/java/edu/coursera/parallel/OneDimAveragingPhaser.java
@@ -108,6 +108,43 @@ public final class OneDimAveragingPhaser {
     public static void runParallelFuzzyBarrier(final int iterations,
             final double[] myNew, final double[] myVal, final int n,
             final int tasks) {
-        runParallelBarrier(iterations, myNew, myVal, n, tasks);
+    	 Phaser ph = new Phaser(0);
+         ph.bulkRegister(tasks);
+
+         Thread[] threads = new Thread[tasks];
+
+         for (int ii = 0; ii < tasks; ii++) {
+             final int i = ii;
+
+             threads[ii] = new Thread(() -> {
+                 double[] threadPrivateMyVal = myVal;
+                 double[] threadPrivateMyNew = myNew;
+
+                 for (int iter = 0; iter < iterations; iter++) {
+                     final int left = i * (n / tasks) + 1;
+                     final int right = (i + 1) * (n / tasks);
+                     int phaser=ph.arrive();
+
+                     for (int j = left; j <= right; j++) {
+                         threadPrivateMyNew[j] = (threadPrivateMyVal[j - 1]
+                             + threadPrivateMyVal[j + 1]) / 2.0;
+                     }
+                     ph.awaitAdvance(phaser);
+
+                     double[] temp = threadPrivateMyNew;
+                     threadPrivateMyNew = threadPrivateMyVal;
+                     threadPrivateMyVal = temp;
+                 }
+             });
+             threads[ii].start();
+         }
+
+         for (int ii = 0; ii < tasks; ii++) {
+             try {
+                 threads[ii].join();
+             } catch (InterruptedException e) {
+                 e.printStackTrace();
+             }
+         }
     }
 }
